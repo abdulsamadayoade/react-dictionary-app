454d46891120e88cd4c0202e2d13a15f23d70e3d
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-01-22 20:00:06 +0100

    add word meaning section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Search from './components/Search'
+import { useState } from "react";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className="dictionary">
       <Header />
       <Search />
+      <Content />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
