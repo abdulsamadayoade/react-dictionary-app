c306f39d82293c870b0246fc1ff8dae4a9e34901
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-01-22 20:12:21 +0100

    update search bar styles

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
+import OutsideClickHandler from "react-outside-click-handler";
 import SearchIcon from "../assets/images/icon-search.svg";
 
 const Search = () => {
-  return (
-    <div className="dictionary__search">
-      <form>
-        <input type="text" placeholder="Search for a word" />
-        <img src={SearchIcon} alt="search icon" />
-      </form>
-    </div>
-  )
-}
+  const [inputActive, setInputActive] = useState(false);
 
-export default Search
+  return (
+    <>
+      <OutsideClickHandler onOutsideClick={() => setInputActive(false)}>
+        <div
+          onClick={() => setInputActive(true)}
+          className={
+            inputActive ? "dictionary__search active" : "dictionary__search"
+          }
+        >
+          <form>
+            <input type="text" placeholder="Search for a word" />
+            <img src={SearchIcon} alt="search icon" />
+          </form>
+        </div>
+      </OutsideClickHandler>
+    </>
+  );
+};
+
+export default Search;
